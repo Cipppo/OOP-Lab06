61c3482b439be83549aa7c0a88dc0219a8c7cfad
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-10 10:20:16 +0100

    Implemented TransactionOverQuotaExc

---
+++ src/it/unibo/oop/lab/exception2/transactionsOverQuotaExceptio.java
@@ -0,0 +1,12 @@
+package it.unibo.oop.lab.exception2;
+
+public class transactionsOverQuotaExceptio extends RuntimeException {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public String toString() {
+		return "The number of ATM transactions gets over the maximum allowed";
+	}
+
+}
