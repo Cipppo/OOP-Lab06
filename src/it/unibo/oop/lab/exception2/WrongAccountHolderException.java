3709bc17b668fef95876d90176dfd5025fc0c2e3
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-14 17:39:01 +0100

    Change from Runtime to IllegalArgument

--- src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
+++ src/it/unibo/oop/lab/exception2/WrongAccountHolderException.java
@@ -2,7 +2,7 @@ package it.unibo.oop.lab.exception2;
 
 
 
-public class WrongAccountHolderException extends RuntimeException{
+public class WrongAccountHolderException extends IllegalArgumentException{
 
 	private static final long serialVersionUID = 1L;
 	
