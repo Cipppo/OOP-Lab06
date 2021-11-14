14fbba19f3eb1dd4ab32f3b5388d5abf080142da
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-14 17:46:58 +0100

    Completed the test cases

--- src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
+++ src/it/unibo/oop/lab/exception2/TestStrictBankAccount.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lab.exception2;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 /**
@@ -23,5 +26,28 @@ public final class TestStrictBankAccount {
          * presenza di un id utente errato, oppure al superamento del numero di
          * operazioni ATM gratuite.
          */
+    	
+    	final AccountHolder usr1 = new AccountHolder("Mario", "Rossi", 1);
+    	final AccountHolder usr2 = new AccountHolder("Luigi", "Bianchi", 2);
+    	
+    	final StrictBankAccount acc1 = new StrictBankAccount(usr1.getUserID(), 10_000, 10);
+    	final StrictBankAccount acc2 = new StrictBankAccount(usr2.getUserID(), 10_000, 10);
+    	
+    	
+    	try {
+    		acc1.deposit(4, 100);
+    		fail();
+    	}catch(WrongAccountHolderException e) {
+    		assertNotNull(e);
+    		e.toString();
+    	}
+    	
+    	for(int i = 0; i < 10; i++) {
+    		try {
+    			acc2.deposit(usr2.getUserID(), 1);
+    		}catch(transactionsOverQuotaExceptio | WrongAccountHolderException e) {
+    			e.getMessage();
+    		}
+    	}
     }
 }
